a31990b4b7ac5da2a1fea970a1150914a0af6354
joaovsj <joaosilva.jesus90@gmail.com>
2024-03-06 13:34:31 -0300

    Migration of the places created

---
+++ database/migrations/2024_03_06_162420_create_places.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('places', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->char('cep', 8); 
+            $table->string('address', 255);
+            $table->integer('number', 10);
+            $table->string('neighborhood', 50);
+            $table->string('city', 50);
+            $table->char('state', 2);
+            $table->string('country', 50);
+            
+            $table->unsignedBigInteger('category_id');
+            $table->timestamps();
+            
+            $tabçe->foreign('category_id')
+                ->references('id')
+                ->on('category')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('places');
+    }
+};
