b290ec26097d055b04bbc4d02f9f154735a12012
joaovsj <joaosilva.jesus90@gmail.com>
2024-03-07 16:05:37 -0300

    Some changes done

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,15 +9,4 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
-
-    // private bool $status   = true;
-    // private array $details = [];
-    
-    // public function output(){
-    //     return [
-    //         'status'    => $this->status,
-    //         'details'   => $this->details
-    //     ];
-    // }
-
 }
